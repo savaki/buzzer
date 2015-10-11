5a51a43a0338578d69852ec2fc0a2d7214c0460f
savaki <matt.ho@gmail.com>
2015-10-11 00:54:50 -0700

    - dumping request values

--- app/controller/home.go
+++ app/controller/home.go
@@ -2,12 +2,21 @@ package controller
 
 import (
 	"encoding/xml"
+	"fmt"
 	"io"
+	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Home(c *gin.Context) {
+	for key, values := range c.Request.Header {
+		fmt.Printf("%s: %s\n", key, strings.Join(values, ", "))
+	}
+	defer c.Request.Body.Close()
+	io.Copy(os.Stdout, c.Request.Body)
+
 	content := TwiML{
 		Say: "hello world",
 	}
