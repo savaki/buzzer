18ed735ea72f32274c26cf217981d7376c700050
savaki <matt.ho@gmail.com>
2015-10-11 18:09:43 -0700

    - hand crafted file server to specify content type

--- app/routes.go
+++ app/routes.go
@@ -1,7 +1,12 @@
 package app
 
 import (
+	"io"
+	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/savaki/buzzer/app/controller"
@@ -18,7 +23,46 @@ func Routes() http.Handler {
 	router.GET("/buzz", controller.Buzz)
 	router.GET("/goodbye", controller.Goodbye)
 
-	router.Static("/static", "static")
+	fs, err := filepath.Abs(".")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	audioContent := func(c *gin.Context) {
+		c.Header("Content-Type", "audio/basic")
+
+		filename, err := filepath.Abs(filepath.Join(fs, c.Request.URL.Path))
+		if err != nil {
+			c.AbortWithStatus(http.StatusNotFound)
+			return
+		}
+
+		log.Println(filename)
+
+		if _, err := os.Stat(filename); err != nil {
+			c.AbortWithStatus(http.StatusNotFound)
+			return
+		}
+
+		log.Println("a")
+
+		// attempted hack, ignore this
+		if !strings.HasPrefix(filename, fs) {
+			c.AbortWithStatus(http.StatusNotFound)
+			return
+		}
+
+		log.Println("b")
+
+		f, err := os.Open(filename)
+		if err != nil {
+			c.AbortWithStatus(http.StatusNotFound)
+			return
+		}
+		defer f.Close()
+
+		io.Copy(c.Writer, f)
+	}
+	router.GET("/static/*path", audioContent)
 
 	return router
 }
